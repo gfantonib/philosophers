cbe291be395657b01b0623116c388178b604696f
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-07-03 17:52:57 -0300

    argument validation implemented

--- include/philo.h
+++ include/philo.h
@@ -6,8 +6,23 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 17:24:21 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/02 17:24:38 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/03 17:39:02 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h> 
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <unistd.h>
+// # include <stdarg.h>
+
+// 01_check_valid_arg.c
+void	check_valid_arg(int argc, char *argv[]);
+
+// utils_1.c
+void	print_message(char *message, int fd);
+int		ft_atoi(const char *str);
+size_t	ft_strlen(const char *s);
+
+#endif
--- source/main.c
+++ source/main.c
@@ -6,23 +6,14 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:46:44 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/02 22:14:49 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/03 17:25:13 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	check_valid_arg(int argc, char *argv[]);
-
 int	main(int argc, char *argv[])
 {
 	check_valid_arg(argc, argv);
 	return (0);
-}
-
-static void	check_valid_arg(int argc, char *argv[])
-{
-	argc = 0;
-	argv = NULL;
-	printf("argc = %d | argv = %p\n", argc, argv);
 }
