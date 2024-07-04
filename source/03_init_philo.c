766bf83fd23c6f536bd8325f19e85bd6b84342db
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-04 17:31:03 -0300

    print_mutex created in program and passed to all philos.

--- source/03_init_philo.c
+++ source/03_init_philo.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 11:04:52 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/04 11:18:55 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/04 16:33:48 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	init_philo(t_philo *philo_array, t_program *program)
 	while (i < program->nbr_of_philo)
 	{
 		philo_array[i].id = i + 1;
+		philo_array[i].print_mtx = &program->print_mtx;
 		i++;
 	}
 }
