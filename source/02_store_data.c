df1a861e5410321bc653b37a9493b5ca85e28f98
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-07-05 14:59:23 -0300

    time_to_eat and time_to_die moved; program->died = 0 added.

--- source/02_store_data.c
+++ source/02_store_data.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 09:24:51 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/05 10:47:43 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/05 14:56:17 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 void	store_data(char *argv[], t_program *program)
 {
 	program->nbr_of_philo = ft_atoi(argv[0]);
-	program->time_to_eat = ft_atoi(argv[1]);
-	program->time_to_die = ft_atoi(argv[2]);
+	program->time_to_die = ft_atoi(argv[1]);
+	program->time_to_eat = ft_atoi(argv[2]);
 	program->time_to_sleep = ft_atoi(argv[3]);
 	program->must_eat = -1;
+	program->died = 0;
 	if (argv[4])
 		program->must_eat = ft_atoi(argv[4]);
 }
