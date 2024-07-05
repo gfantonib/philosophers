9b7a55789eeffa0e25c011af48558e7fca51c2d4
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-07-05 12:27:41 -0300

    eat_even and eat_odd added instead of usleep(500).

--- source/06_philo_routine.c
+++ source/06_philo_routine.c
@@ -6,13 +6,14 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 10:11:39 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/05 10:36:08 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/05 12:26:07 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	eat(t_philo *philo);
+static void	eat_even(t_philo *philo);
+static void	eat_odd(t_philo *philo);
 
 void	*philo_routine(void *arg)
 {
@@ -20,12 +21,13 @@ void	*philo_routine(void *arg)
 	
 	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
-		usleep(500);
-	eat(philo);
+		eat_even(philo);
+	else
+		eat_odd(philo);
 	return (arg);
 }
 
-void	eat(t_philo *philo)
+static void	eat_even(t_philo *philo)
 {
 	pthread_mutex_lock(philo->r_fork);
 	print_state_change("has taken r_fork", philo, get_current_time());
@@ -37,4 +39,18 @@ void	eat(t_philo *philo)
 	philo->is_eating = 0;
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
+}
+
+static void	eat_odd(t_philo *philo)
+{
+	pthread_mutex_lock(philo->l_fork);
+	print_state_change("has taken l_fork", philo, get_current_time());
+	pthread_mutex_lock(philo->r_fork);
+	print_state_change("has taken r_fork", philo, get_current_time());
+	philo->is_eating = 1;
+	print_state_change("is eating", philo, get_current_time());
+	usleep(philo->time_to_eat);
+	philo->is_eating = 0;
+	pthread_mutex_unlock(philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
 }
