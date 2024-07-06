9b4d6d5e946939278c6dc7411265c7bdda1b67a4
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-06 12:38:03 -0300

    philo_array[0].died_mtx instead of philo_array[1].died_mtx on is_dead function

--- source/07_footman_routine.c
+++ source/07_footman_routine.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 12:54:47 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/06 11:34:34 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/06 12:01:00 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,9 @@ static int check_if_dead(t_philo *philo_array)
 		if (is_dead(&philo_array[i]))
 		{
 			print_state_change("died", &philo_array[i], get_current_time());
-			pthread_mutex_lock(philo_array[i].died_mtx);
+			pthread_mutex_lock(philo_array[0].died_mtx);
 			*philo_array[0].died = 1;
-			pthread_mutex_unlock(philo_array[i].died_mtx);
+			pthread_mutex_unlock(philo_array[0].died_mtx);
 			return (1);
 		}
 		i++;
