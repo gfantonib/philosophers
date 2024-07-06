7eadbcae2f0e374e013f59d1dc9281990df14f8a
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-06 15:34:48 -0300

    int eaten seted to 0

--- source/03_init_philo.c
+++ source/03_init_philo.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 11:04:52 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/06 15:01:48 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/06 15:23:02 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	init_philo(t_philo *philo_array, t_program *program)
 		philo_array[i].id = i + 1;
 		philo_array[i].is_eating = 0;
 		philo_array[i].last_meal = get_current_time();
+		philo_array[i].eaten = 0;
 		philo_array[i].died = &program->died;
 		philo_array[i].nbr_of_philo = program->nbr_of_philo;
 		philo_array[i].time_to_eat = program->time_to_eat;
