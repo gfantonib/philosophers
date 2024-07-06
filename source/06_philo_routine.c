ef60449d89714979ca6ac68a644819ec02579681
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-06 11:48:50 -0300

    dead_flag_off added to philo routine

--- source/06_philo_routine.c
+++ source/06_philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 10:11:39 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/06 11:09:32 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/06 11:43:44 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 static void	eat_even(t_philo *philo);
 static void	eat_odd(t_philo *philo);
 static void	dream(t_philo *philo);
+static int	dead_flag_off(t_philo *philo);
 
 void	*philo_routine(void *arg)
 {
 	t_philo			*philo;
 	
 	philo = (t_philo *)arg;
-	while (1)
+	while (dead_flag_off(philo))
 	{
-		if (*philo->died)
-			break;
 		if (philo->id % 2 == 0)
 			eat_even(philo);
 		else
@@ -72,4 +71,14 @@ static void	dream(t_philo *philo)
 {
 	print_state_change("is sleeping", philo, get_current_time());
 	usleep(1000 * philo->time_to_sleep);
+}
+
+static int	dead_flag_off(t_philo *philo)
+{
+	pthread_mutex_lock(philo->died_mtx);
+	if (*philo->died)
+		return (pthread_mutex_unlock(philo->died_mtx), 0);
+	pthread_mutex_unlock(philo->died_mtx);
+	return (1);
+
 }
