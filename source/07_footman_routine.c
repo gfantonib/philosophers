6f9cc33f72b71f98065ec110abf151d370bf5778
gfantoni <guilhermefantoniborges@gmail.com>
2024-07-06 13:36:31 -0300

    check if satisfied partialy implemented: commented

--- source/07_footman_routine.c
+++ source/07_footman_routine.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 12:54:47 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/07/06 12:01:00 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/07/06 13:29:37 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	*footman_routine(void *arg)
 	{
 		if (check_if_dead(philo_array))
 			break ;
+		// else if (check_if_satisfied(philo_array))
+		// 	break ;
 	}
 	return (arg);
 }
@@ -56,3 +58,23 @@ static int	is_dead(t_philo *philo)
 	pthread_mutex_unlock(philo->meal_mtx);
 	return (0);
 }
+
+// static int	check_if_satisfied(t_philo *philo_array)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (i < philo_array[0].nbr_of_philo)
+// 	{
+// 		if (is_dead(&philo_array[i]))
+// 		{
+// 			print_state_change("died", &philo_array[i], get_current_time());
+// 			pthread_mutex_lock(philo_array[0].died_mtx);
+// 			*philo_array[0].died = 1;
+// 			pthread_mutex_unlock(philo_array[0].died_mtx);
+// 			return (1);
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
